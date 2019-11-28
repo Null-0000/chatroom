ea87414df3cc3230c12ea2aa6411e077e09f5849
Furyton <1543159426@qq.com>
2019-11-28 18:12:52 +0800

    为确保隐私，本地用户不需要记录自己的密码，删掉啦啦啦啦 resizingList改为ArrayList

--- src/client/user/UserInfo.java
+++ src/client/user/UserInfo.java
@@ -2,23 +2,19 @@ package client.user;
 
 import client.tools.ResizingList;
 
+import java.util.ArrayList;
+
 public class UserInfo {
     public int ID;
-    private String password;
     public String name;
     public String sig;
-    public ResizingList<String> friends;
+    public ArrayList<String> friends;
 
-    public UserInfo(int ID, String password, String nikeName,
-                    String sig, ResizingList<String> friends) {
+    public UserInfo(int ID, String nikeName,
+                    String sig, ArrayList<String> friends) {
         this.ID = ID;
-        this.password = password;
         this.name = nikeName;
         this.sig = sig;
         this.friends = friends;
-    }
-
-    protected String getPassword() {
-        return password;
     }
 }
